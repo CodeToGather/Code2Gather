50d04c32968ae9b66ebfa4b819b1bd549257bc7b
Hanming Zhu <hanming@huawen.com>
2021-09-18 21:20:36 +0800

    feat: set up precommit hooks

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,55 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:jsx-a11y/strict',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'jsx-a11y', 'simple-import-sort'],
+  ignorePatterns: ['build/**/*.js'],
+  rules: {
+    'linebreak-style': ['error', 'unix'],
+    'no-undef': 'off',
+    'react/jsx-boolean-value': ['error', 'always'],
+    'react/jsx-sort-props': 'error',
+    semi: ['error', 'always'],
+    'simple-import-sort/imports': [
+      'error',
+      {
+        groups: [
+          // Packages. `react` related packages come first.
+          ['^react', '^@?\\w'],
+          // Internal packages.
+          [
+            '^(app|assets|components|constants|contexts|routes|types|utils)(/.*|$)',
+          ],
+          // Side effect imports.
+          ['^\\u0000'],
+          // Parent imports. Put `..` last.
+          ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+          // Other relative imports. Put same-folder imports and `.` behind, and style imports last.
+          ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$', '^.+\\.s?css$'],
+        ],
+      },
+    ],
+    'simple-import-sort/exports': 'error',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
