ffbeaa41a5b0e7e750c0ae45452ce0f78a3a904d
Hanming Zhu <hanming@huawen.com>
2021-09-25 01:33:47 +0800

    refactor(code-executor): update project structure for code-executor to work

--- code-executor/src/code_executor.py
+++ code-executor/src/code_executor.py
@@ -1,26 +1,29 @@
-import json
 import base64
-import requests
+import json
 from pprint import pprint
-from utils.bimap import Bimap
+from typing import Optional, Union
+
+import requests
+
+from src.utils.bimap import Bimap
 
 
-class CodeExecutor(object):
+class CodeExecutor:
     SUBMISSION_RETRIEVAL_URL = "{url}/submissions/{submission_id}?base64_encoded=true"
     SUBMISSIONS_SEND_URL = "{url}/submissions/?base64_encoded=false&wait=false"
     LANGUAGES_URL = "{url}/languages"
 
     JSON_HEADERS = {"Content-Type": "application/json"}
-    TOKEN_KEY = 'token'
-    STATUS_KEY = 'status'
-    ID_KEY = 'id'
-    NAME_KEY = 'name'
-    SOURCE_CODE_KEY = 'source_code'
-    LANGUAGE_ID_KEY = 'language_id'
-    STDIN_KEY = 'stdin'
-    BASE64_RESULTS_FIELDS = ['stderr', 'stdout', 'compile_output', 'message']
+    TOKEN_KEY = "token"
+    STATUS_KEY = "status"
+    ID_KEY = "id"
+    NAME_KEY = "name"
+    SOURCE_CODE_KEY = "source_code"
+    LANGUAGE_ID_KEY = "language_id"
+    STDIN_KEY = "stdin"
+    BASE64_RESULTS_FIELDS = ["stderr", "stdout", "compile_output", "message"]
 
-    def __init__(self, judge_url: str):
+    def __init__(self, judge_url: str) -> None:
         """
         Code Executor.
         Sends code execution instructions to remote Judge0 API
@@ -28,16 +31,18 @@ class CodeExecutor(object):
         if len(judge_url) == 0:
             raise ValueError("URL cannot be empty")
         self._url = judge_url
-        self._languages = self._set_supported_languages()
+        self._languages: Bimap[int, str] = self._set_supported_languages()
 
-    def _create_send_payload(self, code: str, language_id: int, input: str):
+    def _create_send_payload(
+        self, code: str, language_id: int, input: str
+    ) -> dict[str, Union[str, int]]:
         return {
             self.SOURCE_CODE_KEY: code,
             self.LANGUAGE_ID_KEY: language_id,
-            self.STDIN_KEY: input
+            self.STDIN_KEY: input,
         }
 
-    def send_to_execute(self, code: str, language_id: int, input: str) -> str:
+    def send_to_execute(self, code: str, language_id: int, input: str) -> Optional[str]:
         """
         Sends the code to an executor to be executed.
         Language is based on the language id that is provided
@@ -49,66 +54,84 @@ class CodeExecutor(object):
         response = requests.post(
             self.SUBMISSIONS_SEND_URL.format(url=self._url),
             json.dumps(self._create_send_payload(code, language_id, input)),
-            headers=self.JSON_HEADERS)
-        return json.loads(response.content).get(self.TOKEN_KEY, None)
+            headers=self.JSON_HEADERS,
+        )
+        submission_id = json.loads(response.content).get(self.TOKEN_KEY, None)
+        return str(submission_id) if submission_id is not None else None
 
-    def _convert_from_base64(self, result: dict, key: str):
+    def _convert_from_base64(self, result: dict, key: str) -> None:
         """Converts a give key of the result from base 64"""
         if key in result and result[key] is not None:
             result[key] = base64.b64decode(result[key]).decode()
 
+    # TODO: Type the return type more explicitly
     def _get_results(self, submission_id: str) -> dict:
         """Retrieves result from the server 1 time"""
-        response = requests.get(self.SUBMISSION_RETRIEVAL_URL.format(
-            url=self._url, submission_id=submission_id))
-        return json.loads(response.content)
+        response = requests.get(
+            self.SUBMISSION_RETRIEVAL_URL.format(
+                url=self._url, submission_id=submission_id
+            )
+        )
+        return dict(json.loads(response.content))
 
-    def get_results(self, submission_id: str) -> str:
+    # TODO: Type the return type more explicitly
+    def get_results(self, submission_id: str) -> dict:
         """This method will block and return the submission"""
         result = self._get_results(submission_id)
-        while(result.get(self.STATUS_KEY, {}).get(self.ID_KEY, None) == 1):
+        while result.get(self.STATUS_KEY, {}).get(self.ID_KEY, None) == 1:
             result = self._get_results(submission_id)
         for b64_attr in self.BASE64_RESULTS_FIELDS:
             self._convert_from_base64(result, b64_attr)
         return result
 
     def _set_supported_languages(self) -> Bimap:
-        """Returns a Bimap of lanaguage that is supported by the executor."""
-        response = requests.get(self.LANGUAGES_URL.format(url = self._url))
+        """Returns a Bimap of language that is supported by the executor."""
+        response = requests.get(self.LANGUAGES_URL.format(url=self._url))
         languageList = json.loads(response.content)
         return Bimap(
-            list(map(
-                lambda x: (x[self.ID_KEY], x[self.NAME_KEY]), languageList
-            ))
+            list(map(lambda x: (x[self.ID_KEY], x[self.NAME_KEY]), languageList))
         )
 
-    def get_language_from_id(self, id: int) -> str:
+    def get_language_from_id(self, id: int) -> Optional[str]:
         """
         Gets the programming language from the id.
         Returns None is the id is not found
         """
         return self._languages.get_value_from_key(id)
 
-    def get_id_from_language(self, language: int) -> str:
+    def get_id_from_language(self, language: str) -> Optional[int]:
         """
         Gets the id of the language from the name
         Returns None if the language is not found
         """
         return self._languages.get_key_from_value(language)
 
-    def get_supported_languages(self) -> list:
+    def get_supported_languages(self) -> list[str]:
         """Get all the programming languages that are supported"""
         return list(self._languages.get_values())
 
 
-if __name__ == "__main__":
+def main():
     URL = ""
     codeExecutor = CodeExecutor(URL)
     language = codeExecutor.get_supported_languages()[-9]
     lang_id = codeExecutor.get_id_from_language(language)
     print(f"{language}: {lang_id}")
+    if lang_id is None:
+        return
+
     submission_id = codeExecutor.send_to_execute(
-        "import ctypes\nx = ctypes.c_double.from_param(1e300)\nrepr(x)", lang_id, "hello\n")
+        "import ctypes\nx = ctypes.c_double.from_param(1e300)\nrepr(x)",
+        lang_id,
+        "hello\n",
+    )
     print(submission_id)
+    if submission_id is None:
+        return
+
     results = codeExecutor.get_results(submission_id)
     pprint(results)
+
+
+if __name__ == "__main__":
+    main()
--- code-executor/src/utils/bimap.py
+++ code-executor/src/utils/bimap.py
@@ -1,34 +1,39 @@
-from typing import Any
+from typing import Generic, Optional, TypeVar
+
+KT = TypeVar("KT")
+VT = TypeVar("VT")
 
 
-class Bimap(object):
-    def __init__(self, entries: list[tuple[Any, Any]]):
+class Bimap(Generic[KT, VT]):
+    def __init__(self, entries: list[tuple[KT, VT]]) -> None:
         """
-        A bidirectional map for lookup
-        Not meant to be mutated
+        A bidirectional map for lookup.
+        Not meant to be mutated.
+
+        Invariant: Bijective key-value pairings.
         """
-        self._forward_map = {}
-        self._reverse_map = {}
+        self._forward_map: dict[KT, VT] = {}
+        self._reverse_map: dict[VT, KT] = {}
         for key, value in entries:
             self._forward_map[key] = value
             self._reverse_map[value] = key
 
-    def get_keys(self) -> Any:
+    def get_keys(self) -> list[KT]:
         """Get all the keys"""
-        return self._forward_map.keys()
+        return list(self._forward_map.keys())
 
-    def get_values(self) -> Any:
+    def get_values(self) -> list[VT]:
         """Get all the values"""
-        return self._reverse_map.keys()
+        return list(self._reverse_map.keys())
 
-    def get_value_from_key(self, key: Any) -> Any:
+    def get_value_from_key(self, key: KT) -> Optional[VT]:
         """
         Gets the value from a key.
         Returns None if the key is not found.
         """
         return self._forward_map.get(key, None)
 
-    def get_key_from_value(self, value: Any) -> Any:
+    def get_key_from_value(self, value: VT) -> Optional[KT]:
         """
         Get a key from a value
         Returns None if the value is not found.
