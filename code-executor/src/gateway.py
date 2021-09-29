f06d20175fbad0b1efff946242e1892af2a89cfa
Wen Junhua <wenjh1998@gmail.com>
2021-09-29 15:46:32 +0800

    feat(code-executor): add more endpoints

--- code-executor/src/gateway.py
+++ code-executor/src/gateway.py
@@ -1,30 +1,61 @@
-from flask import Flask, jsonify, request
+from typing import Any, Union
+
+from flask import Blueprint, Response, g, json, jsonify, request
 
 from src.code_executor import CodeExecutor
-from src.constants import URL
+from src.constants import CODE_KEY, INPUT_KEY, LANGUAGE_KEY, URL
 
-app = Flask(__name__)
-executor = CodeExecutor(URL)
+executor_blueprint = Blueprint("/", __name__)
 
 
-@app.route("/languages")
-def get_languages():
+def get_executor() -> Union[Any, CodeExecutor]:
+    """
+    Returns the Code executor in current context.
+    """
+    if "executor" not in g:
+        g.executor = CodeExecutor(URL)
+    return g.executor
+
+
+@executor_blueprint.route("/languages")
+def get_languages() -> Response:
     """
     Gets all the different supported languages.
-    Returns a json list as a response
+    Returns a json list as a response.
     """
-    return jsonify(executor.get_supported_languages())
+    return jsonify(get_executor().get_supported_languages())
 
 
-@app.route("/submission", methods="POST")
-def submission():
+@executor_blueprint.route("/submission", methods=["POST"])
+def submission() -> Response:
     """
-    Execute code and returns a submission tag
+    Execute code and returns a submission tag.
     """
     data = request.get_json()
-    result = executor.send_to_execute(**data)
+
+    if data is None:
+        return jsonify({"error": "invalid request"})
+
+    # Check for valid args
+    code = data.get(CODE_KEY, None)
+    language = data.get(LANGUAGE_KEY, None)
+    stdin = data.get(INPUT_KEY, None)
+    if None in (code, language):
+        return jsonify({"error": "missing either code, language or input"})
+
+    executor = get_executor()
+    language_id = executor.get_id_from_language(language)
+    if language_id is None:
+        return jsonify({"error": "language is not found"})
+
+    result = executor.send_to_execute(code, language_id, stdin)
     return jsonify({"result": result})
 
 
-if __name__ == "__main__":
-    app.run(debug=True, host="", port=8000)
+@executor_blueprint.route("/submissions/<path:path>")
+def get_submission(path: str) -> Response:
+    """
+    Gets the output of the results.
+    """
+    results = get_executor().get_results(path)
+    return jsonify(results)
