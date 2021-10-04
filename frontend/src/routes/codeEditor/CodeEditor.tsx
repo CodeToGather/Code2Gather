5abf21aa2c83eaad31c3953e31dde9475b73b594
Hanming Zhu <hanming@huawen.com>
2021-10-04 17:35:25 +0800

    feat(frontend): improve styling for code editor

--- frontend/src/routes/codeEditor/CodeEditor.tsx
+++ frontend/src/routes/codeEditor/CodeEditor.tsx
@@ -8,27 +8,40 @@ import 'ace-builds/src-noconflict/mode-javascript';
 import './theme-twilight';
 import 'ace-builds/src-noconflict/ext-language_tools';
 
+import LanguageDropdown, { ProgrammingLanguage } from './LanguageDropdown';
 import './CodeEditor.scss';
 
 const CodeEditor: FC = () => {
-  const [language, setLanguage] = useState('python');
+  const [language, setLanguage] = useState<ProgrammingLanguage>(
+    ProgrammingLanguage.PYTHON,
+  );
 
   return (
-    <div>
-      <div className="select">
-        <select onBlur={(e) => setLanguage(e.target.value)}>
-          <option value="python">Python</option>
-          <option value="javascript">JavaScript</option>
-          <option value="java">Java</option>
-        </select>
+    <div className="editor">
+      <div className="editor--top">
+        <div className="editor--top__left-buttons">
+          <LanguageDropdown
+            className="editor--top__language-button"
+            language={language}
+            setLanguage={setLanguage}
+          />
+          <button className="border-button editor--top__help-button">
+            Help <i className="far fa-question-circle" />
+          </button>
+        </div>
+        <div className="editor--top__right-buttons">
+          <button className="border-button is-danger editor--top__leave-button">
+            Leave Room
+          </button>
+        </div>
       </div>
       <AceEditor
-        height={'100vh'}
         mode={language}
         name="code-editor"
         showPrintMargin={false}
         theme="twilight"
         width={'100vw'}
+        wrapEnabled={true}
       />
     </div>
   );
