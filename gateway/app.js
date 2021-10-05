ba1c2771324390032ec649047bb98df403e9c28b
eksinyue <xinyuehehxy@gmail.com>
2021-10-05 21:58:33 +0800

    build(gateway): remove unused dependencies

--- gateway/app.js
+++ gateway/app.js
@@ -1,7 +1,5 @@
 const code = require('http-status-codes');
 const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
 
@@ -15,8 +13,6 @@ app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 // middleware: auth service
 app.use('/', checkAuth);
