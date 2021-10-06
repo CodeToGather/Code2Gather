c39857c93e069ab17908c2e25581d27f9330aa2d
Hanming Zhu <hanming@huawen.com>
2021-10-06 23:28:09 +0800

    test(frontend): fix landing page test

--- frontend/src/routes/landing/Landing.test.tsx
+++ frontend/src/routes/landing/Landing.test.tsx
@@ -1,9 +1,24 @@
+import { ReactElement } from 'react';
 import { render, screen } from '@testing-library/react';
+
+import { AuthContext } from 'contexts/AuthContext';
+import { emptyPromiseFunction } from 'utils/functionUtils';
 
 import Landing from './Landing';
 
+// Renders the UI within an AuthContext.
+const contextRender = (ui: ReactElement, login: () => Promise<void>) => {
+  return render(
+    <AuthContext.Provider
+      value={{ data: null, logout: emptyPromiseFunction, login }}
+    >
+      {ui}
+    </AuthContext.Provider>,
+  );
+};
+
 test('renders mock interviews text', () => {
-  render(<Landing />);
+  contextRender(<Landing />, emptyPromiseFunction);
   const mockInterviewsTextElement = screen.getByText(
     /mock interviews made easier./i,
   );
@@ -11,7 +26,7 @@ test('renders mock interviews text', () => {
 });
 
 test('renders excel text', () => {
-  render(<Landing />);
+  contextRender(<Landing />, emptyPromiseFunction);
   const excelTextElement = screen.getByText(
     /excel in your technical interviews today./i,
   );
@@ -19,19 +34,30 @@ test('renders excel text', () => {
 });
 
 test('renders github button', () => {
-  render(<Landing />);
+  contextRender(<Landing />, emptyPromiseFunction);
   const githubButtonElement = screen.getByText(/sign in with github/i);
   expect(githubButtonElement).toBeInTheDocument();
 });
 
+test('renders github button that logins via auth context', () => {
+  let hasTriggeredLogin = false;
+  const login = async (): Promise<void> => {
+    hasTriggeredLogin = true;
+  };
+  contextRender(<Landing />, login);
+  const githubButtonElement = screen.getByText(/sign in with github/i);
+  githubButtonElement.click();
+  expect(hasTriggeredLogin).toBe(true);
+});
+
 test('renders guest button', () => {
-  render(<Landing />);
+  contextRender(<Landing />, emptyPromiseFunction);
   const guestButtonElement = screen.getByText(/sign in as guest instead/i);
   expect(guestButtonElement).toBeInTheDocument();
 });
 
 test('renders demo image', () => {
-  render(<Landing />);
+  contextRender(<Landing />, emptyPromiseFunction);
   const imageElement = screen.getByAltText(/code2gather demo/i);
   expect(imageElement).toBeInTheDocument();
 });
---
+++ frontend/src/utils/functionUtils.ts
@@ -0,0 +1,3 @@
+export const emptyFunction = (): undefined => undefined;
+
+export const emptyPromiseFunction = (): Promise<void> => Promise.resolve();
