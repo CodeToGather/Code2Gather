ba77da2a68650165b13aff41bba56c481b1417fb
Hanming Zhu <hanming@huawen.com>
2021-10-07 20:20:12 +0800

    test(history): add test utilities

---
+++ history/src/utils/tests.ts
@@ -0,0 +1,172 @@
+import faker from 'faker';
+
+import prisma from 'lib/prisma';
+import { Difficulty, Language } from 'types/crud/enums';
+import { MeetingRecordCreateData } from 'types/crud/meetingRecord';
+import { RatingCreateData } from 'types/crud/rating';
+import { UserCreateData } from 'types/crud/user';
+
+import { MeetingRecord, Rating, User } from '.prisma/client';
+
+const checkTestEnv = (): void => {
+  if (process.env.NODE_ENV !== 'test') {
+    throw new Error('You can only run this in a test environment.');
+  }
+};
+
+export const convertDatesToJson = (object: any): any => {
+  if (object == null) {
+    return object;
+  }
+  if (Array.isArray(object)) {
+    return object.map((obj) => convertDatesToJson(obj));
+  }
+  if (typeof object !== 'object') {
+    return object;
+  }
+  if (object instanceof Date) {
+    return object.toJSON();
+  }
+  const result: Record<string, unknown> = {};
+  for (const [key, value] of Object.entries(object)) {
+    result[key] = convertDatesToJson(value);
+  }
+  return result;
+};
+
+// Resets the database.
+// Can only be called in test environment.
+export const resetDatabase = async (): Promise<void> => {
+  checkTestEnv();
+  await prisma.user.deleteMany({});
+  await prisma.rating.deleteMany({});
+  await prisma.meetingRecord.deleteMany({});
+};
+
+// Creates a test user and persists it into the database.
+// Can only be called in test environment.
+export const createTestUser = async (
+  data?: Partial<UserCreateData>
+): Promise<User> => {
+  checkTestEnv();
+  return await prisma.user.create({
+    data: {
+      id: data?.id ?? faker.random.alphaNumeric(20),
+      githubUsername: data?.githubUsername ?? faker.random.alphaNumeric(20),
+    },
+  });
+};
+
+// Returns a user object that is not persisted into the database.
+// Can only be called in test environment.
+export const mockTestUser = (data?: Partial<UserCreateData>): User => {
+  checkTestEnv();
+  return {
+    id: data?.id ?? faker.random.alphaNumeric(20),
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    githubUsername: data?.githubUsername ?? faker.random.alphaNumeric(20),
+  };
+};
+
+// Creates a test rating and persists it into the database.
+// Can only be called in test environment.
+export const createTestRating = async (
+  data: Partial<RatingCreateData>
+): Promise<Rating> => {
+  checkTestEnv();
+  let finalRatingUserId = data.ratingUserId;
+  let finalRatedUserId = data.ratedUserId;
+  if (finalRatingUserId == null) {
+    finalRatingUserId = (await createTestUser()).id;
+  }
+  if (finalRatedUserId == null) {
+    finalRatedUserId = (await createTestUser()).id;
+  }
+  return await prisma.rating.create({
+    data: {
+      ratingUserId: finalRatingUserId,
+      ratedUserId: finalRatedUserId,
+      rating: data.rating ?? faker.datatype.number({ min: 1, max: 5 }),
+    },
+  });
+};
+
+// Returns a rating object that is not persisted into the database.
+// Can only be called in test environment.
+export const mockTestRating = (
+  ratingUserId: string,
+  ratedUserId: string,
+  data?: Partial<Omit<Omit<RatingCreateData, 'ratingUserId'>, 'ratedUserId'>>
+): Rating => {
+  checkTestEnv();
+  return {
+    id: faker.datatype.number(),
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    ratingUserId,
+    ratedUserId,
+    rating: data?.rating ?? faker.datatype.number({ min: 1, max: 5 }),
+  };
+};
+
+// Creates a test meeting record and persists it into the database.
+// Can only be called in test environment.
+export const createTestMeetingRecord = async (
+  data: Partial<MeetingRecordCreateData>
+): Promise<MeetingRecord> => {
+  checkTestEnv();
+  let finalInterviewerId = data.interviewerId;
+  let finalIntervieweeId = data.intervieweeId;
+  if (finalInterviewerId == null) {
+    finalInterviewerId = (await createTestUser()).id;
+  }
+  if (finalIntervieweeId == null) {
+    finalIntervieweeId = (await createTestUser()).id;
+  }
+  return await prisma.meetingRecord.create({
+    data: {
+      interviewerId: finalInterviewerId,
+      intervieweeId: finalIntervieweeId,
+      duration: data.duration ?? faker.datatype.float(),
+      questionId: data.questionId ?? faker.random.alpha(),
+      questionDifficulty:
+        data.questionDifficulty ??
+        faker.random.arrayElement(Object.values(Difficulty)),
+      language:
+        data.language ?? faker.random.arrayElement(Object.values(Language)),
+      codeWritten: data.codeWritten ?? faker.random.alphaNumeric(),
+      isSolved: data.isSolved ?? faker.datatype.boolean(),
+      feedbackToInterviewee:
+        data.feedbackToInterviewee ?? faker.lorem.paragraph(),
+    },
+  });
+};
+
+export const mockTestMeetingRecord = (
+  interviewerId: string,
+  intervieweeId: string,
+  data?: Partial<
+    Omit<Omit<MeetingRecordCreateData, 'interviewerId'>, 'intervieweeId'>
+  >
+): MeetingRecord => {
+  checkTestEnv();
+  return {
+    id: faker.datatype.number(),
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    interviewerId,
+    intervieweeId,
+    duration: data?.duration ?? faker.datatype.float(),
+    questionId: data?.questionId ?? faker.random.alpha(),
+    questionDifficulty:
+      data?.questionDifficulty ??
+      faker.random.arrayElement(Object.values(Difficulty)),
+    language:
+      data?.language ?? faker.random.arrayElement(Object.values(Language)),
+    codeWritten: data?.codeWritten ?? faker.random.alphaNumeric(),
+    isSolved: data?.isSolved ?? faker.datatype.boolean(),
+    feedbackToInterviewee:
+      data?.feedbackToInterviewee ?? faker.lorem.paragraph(),
+  };
+};
