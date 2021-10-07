c05f60bb47e42d104a7211d19502d06b6a956fef
Hanming Zhu <hanming@huawen.com>
2021-10-07 20:17:27 +0800

    feat(history): add meeting record-related policies

---
+++ history/src/policies/meetingRecord/AllowIfUserIsInterviewerPolicy.ts
@@ -0,0 +1,22 @@
+import { Authorization } from 'types/authorization';
+import { MeetingRecordCreateData } from 'types/crud/meetingRecord';
+
+import { BasePolicy } from '../BasePolicy';
+
+import { MeetingRecord, User } from '.prisma/client';
+
+// Checks that the user is the owner of the post before
+// allowing for mutations to said post.
+export class AllowIfUserIsInterviewerPolicy extends BasePolicy<
+  MeetingRecord | MeetingRecordCreateData
+> {
+  public async validate(
+    item: MeetingRecord | MeetingRecordCreateData,
+    user: User
+  ): Promise<Authorization> {
+    if (item.interviewerId === user.id) {
+      return Authorization.ALLOW;
+    }
+    return Authorization.SKIP;
+  }
+}
