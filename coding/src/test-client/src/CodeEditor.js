516ae0894638d04cc5c0703596e5c923c98cbd98
Wen Junhua <wenjh1998@gmail.com>
2021-10-30 11:05:20 +0800

    fix(coding): update current codemirror changes

--- coding/src/test-client/src/CodeEditor.js
+++ coding/src/test-client/src/CodeEditor.js
@@ -1,66 +1,73 @@
-import { useEffect, useState } from 'react';
-import { io } from 'socket.io-client';
-import P2P from 'socket.io-p2p';
-import CodeMirror from '@uiw/react-codemirror';
+import { useEffect, useState, useRef } from 'react';
+import * as Y from 'yjs';
+import { CodemirrorBinding } from 'y-codemirror';
+import { WebrtcProvider } from 'y-webrtc';
+import { useCodeMirror } from '@uiw/react-codemirror';
 import 'codemirror/lib/codemirror.css';
 import 'codemirror/theme/monokai.css';
+import { javascript } from '@codemirror/lang-javascript';
 import './EditorAddons';
+import RandomColor from 'randomcolor';
 
 export default function CodeEditor() {
   const hasLineNo = true;
+  const roomName = 'name';
+  const currentUser = 'user';
   const [code, setCode] = useState();
-  const [socket, setSocket] = useState();
+  const editor = useRef();
+  const { container, setContainer } = useCodeMirror({
+    container: editor.current,
+    options: {
+      lineNumbers: hasLineNo,
+    },
+    extensions: [javascript()],
+    value: code,
+    onchange: (e, data, value) => {
+      setCode(value);
+    },
+  });
+  let binding;
 
   useEffect(() => {
-    const s = io('http://localhost:3001');
-    const p2p = new P2P(s);
-    setSocket(p2p);
-    return () => {
-      s.disconnect();
-    };
-  }, []);
-
-  useEffect(() => {
-    if (socket) {
-      socket.on('connect', () => {
-        socket.emit('join', 'code-editor');
-      });
-      socket.on('code', (c) => {
-        setCode(c);
-      });
+    if (editor.current != null) {
+      setContainer(editor.current);
     }
-  }, [socket]);
+  }, [editor.current]);
+
+  useEffect(() => {
+    if (editor.current == null || container == null) return {};
+    console.log(container.firstChild.getDoc());
+    const sharedDoc = new Y.Doc();
+    let provider = null;
+
+    provider = new WebrtcProvider(roomName, sharedDoc, {
+      signaling: ['wss://localhost:3001'],
+    });
+    const text = sharedDoc.getText('key');
+    const undoManager = new Y.UndoManager(text);
+
+    provider.awareness.setLocalStateField('user', {
+      name: currentUser,
+      color: RandomColor(),
+    });
+    binding = new CodemirrorBinding(text, editor.current, provider.awareness, {
+      undoManager,
+    });
+
+    return () => {
+      if (provider == null) return;
+      binding.destroy();
+      provider.destroy();
+      sharedDoc.destroy();
+    };
+  }, [editor.current]);
 
   return (
     <div className="App">
       <header className="App-header"></header>
       <div className="absolute top-20 bottom-40 left-10 right-10 text-left">
         <h1>Welcome to the code editor</h1>
-        <CodeMirror
-          value={code}
-          theme="monokai"
-          onChange={(value) => {
-            console.log(value);
-            setCode(value);
-            socket.emit('code', value);
-          }}
-          autoScroll
-          options={{
-            mode: 'text/x-python',
-            lineNumbers: hasLineNo,
-            lineWrapping: true,
-            smartIndent: true,
-            foldGutter: true,
-            tabSize: 2,
-            gutters: ['CodeMirror-linenumbers', 'CodeMirror-foldgutter'],
-            autoCloseTags: true,
-            matchBrackets: true,
-            autoCloseBrackets: true,
-            extraKeys: {
-              'Ctrl-Space': 'autocomplete',
-            },
-          }}
-        />
+        <div ref={editor} />
       </div>
     </div>
   );
