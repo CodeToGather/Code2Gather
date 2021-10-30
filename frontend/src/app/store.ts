285fe5f823ff49002fae3148a059534c7fe63a7b
Hanming Zhu <hanming@huawen.com>
2021-10-30 22:13:05 +0800

    feat(frontend): add redux to app

---
+++ frontend/src/app/store.ts
@@ -0,0 +1,33 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+import { configureStore } from '@reduxjs/toolkit';
+import rootReducer from 'reducers/rootReducer';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+  key: 'code2gather',
+  storage,
+};
+
+// To support the persisting of redux across sessions
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// Creation of redux store
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    // Creation of custom middleware, needed for redux persist
+    getDefaultMiddleware({ serializableCheck: false }),
+});
+
+export const persistor = persistStore(store);
+
+// Settings to aid development
+if (process.env.NODE_ENV === 'development' && module.hot) {
+  module.hot.accept('reducers/rootReducer', () => {
+    const newRootReducer = require('reducers/rootReducer').default;
+    store.replaceReducer(newRootReducer);
+  });
+}
+
+export default store;
