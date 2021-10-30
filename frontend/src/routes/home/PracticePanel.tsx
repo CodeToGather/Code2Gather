bfbb2fef2544cb9552b135b4daaee973215c9904
Hanming Zhu <hanming@huawen.com>
2021-10-30 15:48:49 +0800

    feat(frontend): add practice panel

--- frontend/src/routes/home/PracticePanel.tsx
+++ frontend/src/routes/home/PracticePanel.tsx
@@ -1,7 +1,39 @@
 import { FC } from 'react';
 
+import Typography from 'components/typography';
+
+import './Home.scss';
+
 const PracticePanel: FC = () => {
-  return <></>;
+  return (
+    <div className="practice-panel">
+      <Typography className="is-bold practice-panel__title" size="medium">
+        Practice
+      </Typography>
+      <Typography className="practice-panel__instruction" size="regular">
+        First, select a question difficulty:
+      </Typography>
+      <button className="border-button practice-panel__button">
+        <Typography size="regular">Easy Question</Typography>
+      </button>
+      <button className="border-button practice-panel__button">
+        <Typography size="regular">Medium Question</Typography>
+      </button>
+      <button className="border-button practice-panel__button">
+        <Typography size="regular">Hard Question</Typography>
+      </button>
+      <Typography className="practice-panel__instruction bottom" size="regular">
+        Then, just press &quot;Practice Now&quot; and we will find someone to do
+        a mock interview with you!
+      </Typography>
+      <button className="primary-button practice-panel__button">
+        Practice Now
+      </button>
+      <button className="secondary-button practice-panel__button new-to-process">
+        I&apos;m new to the process.
+      </button>
+    </div>
+  );
 };
 
 export default PracticePanel;
