e0d0d5855069adb185e6bd61d1ad150c72f53619
eksinyue <xinyuehehxy@gmail.com>
2021-11-07 00:24:05 +0800

    chore(room): send uid as path param to get user info from history service

--- room/src/server/http_client/http_client.go
+++ room/src/server/http_client/http_client.go
@@ -32,10 +32,8 @@ func (c *HttpClient) Post(path string, data []byte) ([]byte, error) {
 	return respBody, err
 }
 
-func (c *HttpClient) Get(path string, data []byte) ([]byte, error) {
-	dataBuffer := bytes.NewBuffer(data)
-
-	req, err := http.NewRequest("GET", path, dataBuffer)
+func (c *HttpClient) Get(path string) ([]byte, error) {
+	req, err := http.NewRequest("GET", path, nil)
 	if err != nil {
 		return nil, err
 	}
--- room/src/server/http_client/http_request.go
+++ room/src/server/http_client/http_request.go
@@ -1,10 +1,11 @@
 package http_client
 
 import (
-	"code2gather.com/room/src/server/util"
+	"encoding/json"
 	"log"
 
 	"code2gather.com/room/src/models"
+	"code2gather.com/room/src/server/util"
 )
 
 var httpClient *HttpClient
@@ -14,37 +15,24 @@ func init() {
 }
 
 func GetUserId(token string) (uid string, err error) {
-	//resp, err := httpClient.GetWithAuthHeader(AuthBaseUrl+"/auth", token)
-	//var responseMessage *models.AuthResponse
-	//err = json.Unmarshal(resp, &responseMessage)
-	//if err != nil {
-	//	return
-	//}
-	//uid = responseMessage.Uid
-	uid = token
+	resp, err := httpClient.GetWithAuthHeader(AuthBaseUrl+"/auth", token)
+	var responseMessage *models.AuthResponse
+	err = json.Unmarshal(resp, &responseMessage)
+	if err != nil {
+		return
+	}
+	uid = responseMessage.Uid
 	return
 }
 
 func GetUserInfo(uid string) (user *models.User, err error) {
-	//request := models.GetUserRequest{Uid: uid}
-	//data, err := util.MarshalToJson(&request)
-	//if err != nil {
-	//	log.Println(err)
-	//	return
-	//}
-	//resp, err := httpClient.Get(HistoryBaseUrl+"/", data)
-	//if err != nil {
-	//	return
-	//}
-	//err = json.Unmarshal(resp, &user)
-	//if err != nil {
-	//	return
-	//}
-	user = &models.User{
-		Id:             "test2",
-		GithubUsername: "testtest",
-		PhotoUrl:       "test",
-		ProfileUrl:     "test",
+	resp, err := httpClient.Get(HistoryBaseUrl + "/user/" + uid)
+	if err != nil {
+		return
+	}
+	err = json.Unmarshal(resp, &user)
+	if err != nil {
+		return
 	}
 	return
 }
