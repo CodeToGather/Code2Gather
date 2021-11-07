d454586d82609ae5425bf5025816e18445b67caf
eksinyue <xinyuehehxy@gmail.com>
2021-11-08 06:27:38 +0800

    feat(room): add get question http route

--- room/src/server/http/http_connection.go
+++ room/src/server/http/http_connection.go
@@ -19,12 +19,14 @@ func StartHttpServer() {
 
 	r.POST("/create", RoomCreationHandler)
 	r.GET("/", CheckInRoomHandler)
+	r.GET("/question/:Qid", GetQuestionHandler)
+
 	r.GET("/roomws/:Token", func(c *gin.Context) {
 		authToken := c.Params.ByName("Token")
 		socket.WSHandler(c.Writer, c.Request, authToken)
 	})
 
-	err := r.Run(":" + ConnPort)
+	err := r.Run("localhost:" + ConnPort)
 	if err != nil {
 		log.Fatalf("Error starting server: %v", err)
 		return
