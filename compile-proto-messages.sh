014d98bfb1660b62288e4319290c983b590560b0
Asthenosphere <58675456+Asthenosphere@users.noreply.github.com>
2021-11-08 00:21:48 +0800

    chore: update script to generate proto message in typescript
    Updated compile-proto-messages.sh to generate message in typescript

--- compile-proto-messages.sh
+++ compile-proto-messages.sh
@@ -8,19 +8,18 @@
 #echo "Compiling message.proto in Python Successfully."
 
 
-#echo "Compiling message.proto in JavaScript ..."
+echo "Compiling message.proto in JavaScript ..."
 
 # TODO: Add other services accordingly
 # for js projects:
 # protoc -I=./proto-messages --js_out=import_style=commonjs,binary:path/to/output/dir ./proto-messages/*.proto
 # e.g.
-#protoc -I=./proto-messages --js_out=import_style=commonjs,binary:auth ./proto-messages/*.proto
-#echo "Compiling message.proto in JavaScript Successfully."
+protoc -I=./proto-messages --ts_out=frontend/src/types/protobuf ./proto-messages/*.proto
+echo "Compiling message.proto in JavaScript Successfully."
 
-
-echo "Compiling message.proto in Go ..."
-export PATH=$PATH:$HOME/go/bin
-export PATH=$PATH:/usr/local/go/bin
-protoc -I=./proto-messages --go_out=./room/src ./proto-messages/*.proto
-echo "Compiling message.proto in Go Successfully."
+# echo "Compiling message.proto in Go ..."
+# export PATH=$PATH:$HOME/go/bin
+# export PATH=$PATH:/usr/local/go/bin
+# protoc -I=./proto-messages --go_out=./room/src ./proto-messages/*.proto
+# echo "Compiling message.proto in Go Successfully."
 
