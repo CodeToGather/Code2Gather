9370f5dceafe7ab769c0bf746a80115f4d63b0cf
eksinyue <xinyuehehxy@gmail.com>
2021-11-08 02:22:24 +0800

    feat(room): seed db with actual questions

--- room/src/main.go
+++ room/src/main.go
@@ -4,11 +4,18 @@ import (
 	"log"
 
 	"code2gather.com/room/src/infra/db"
+	"code2gather.com/room/src/infra/db/db_seed"
 	"code2gather.com/room/src/server/http"
 )
 
 func main() {
-	err := db.ConnectoToDB()
+	err := db_seed.SeedDB()
+	if err != nil {
+		log.Fatalf("Error seeding db: %v", err)
+		return
+	}
+
+	err = db.ConnectoToDB()
 	if err != nil {
 		log.Fatalf("Error connecting to db: %v", err)
 		return
