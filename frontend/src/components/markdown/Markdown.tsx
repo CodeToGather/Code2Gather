3f0671335a22bf7c0444a303592cf2af94346307
Hanming Zhu <hanming@huawen.com>
2021-11-07 14:57:19 +0800

    chore: remove unnecessary markdown packages

--- frontend/src/components/markdown/Markdown.tsx
+++ frontend/src/components/markdown/Markdown.tsx
@@ -1,10 +1,7 @@
 import { FC } from 'react';
 import ReactMarkdown from 'react-markdown';
-import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 import rehypeKatex from 'rehype-katex';
 import rehypePrismPlus from 'rehype-prism-plus';
-import rehypeSlug from 'rehype-slug';
-import remarkFootnotes from 'remark-footnotes';
 import remarkGfm from 'remark-gfm';
 import remarkMath from 'remark-math';
 
@@ -18,17 +15,8 @@ interface Props {
 const Markdown: FC<Props> = ({ content, className = '' }) => (
   <div className={`markdown${className ? ` ${className}` : ''}`}>
     <ReactMarkdown
-      rehypePlugins={[
-        rehypeSlug,
-        rehypeAutolinkHeadings,
-        rehypeKatex,
-        [rehypePrismPlus, { ignoreMissing: true }],
-      ]}
-      remarkPlugins={[
-        [remarkGfm],
-        [remarkFootnotes, { inlineNotes: true }],
-        remarkMath,
-      ]}
+      rehypePlugins={[rehypeKatex, [rehypePrismPlus, { ignoreMissing: true }]]}
+      remarkPlugins={[[remarkGfm], remarkMath]}
     >
       {content}
     </ReactMarkdown>
