ace0461e6f489ffc8088c00c2957919e3f85e49c
Hanming Zhu <hanming@huawen.com>
2021-12-05 18:40:17 +0800

    feat(frontend): disable guest room route when app is disabled

--- frontend/src/app/UnauthenticatedApp.tsx
+++ frontend/src/app/UnauthenticatedApp.tsx
@@ -6,6 +6,7 @@ import {
   Switch,
 } from 'react-router-dom';
 
+import { IS_APP_DISABLED } from 'constants/config';
 import { GUEST, HELP, ROOT } from 'constants/routes';
 import Guest from 'routes/guest';
 import Help from 'routes/help';
@@ -18,11 +19,13 @@ const UnauthenticatedApp: FC = () => {
     <Router>
       <Switch>
         <Route component={Landing} exact={true} path={ROOT} />
-        <Route
-          exact={true}
-          path={`${GUEST}/:id`}
-          render={(props): ReactElement<typeof Guest> => <Guest {...props} />}
-        />
+        {!IS_APP_DISABLED && (
+          <Route
+            exact={true}
+            path={`${GUEST}/:id`}
+            render={(props): ReactElement<typeof Guest> => <Guest {...props} />}
+          />
+        )}
         <Route component={Help} exact={true} path={HELP} />
         <Route path="/" render={redirectToRoot} />
       </Switch>
