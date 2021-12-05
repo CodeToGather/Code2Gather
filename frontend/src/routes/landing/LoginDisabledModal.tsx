691ba66a02b778ad09be8e3c7536a51445adce33
Hanming Zhu <hanming@huawen.com>
2021-12-05 18:19:04 +0800

    feat(frontend): add logind disabled modal

---
+++ frontend/src/routes/landing/LoginDisabledModal.tsx
@@ -0,0 +1,38 @@
+import { FC } from 'react';
+
+import Typography from 'components/typography';
+import { HELP } from 'constants/routes';
+
+interface Props {
+  onClose: () => void;
+}
+
+const LoginDisabledModal: FC<Props> = ({ onClose }) => {
+  return (
+    <>
+      <Typography className="is-bold" size="large">
+        Login is currently disabled.
+      </Typography>
+      <Typography className="modal-instruction" size="regular">
+        The application is currently unavailable for usage. This may be due to
+        maintenance or due to services taken down for costs saving purposes.
+        We&apos;re really sorry for any inconvenience caused.
+      </Typography>
+      <div className="modal-buttons">
+        <button className="border-button" onClick={onClose}>
+          <Typography size="regular">Close</Typography>
+        </button>
+        <button
+          className="border-button is-success"
+          onClick={(): void => {
+            window.location.href = HELP;
+          }}
+        >
+          <Typography size="regular">Check out the Help page</Typography>
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default LoginDisabledModal;
